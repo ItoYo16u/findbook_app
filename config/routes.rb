74c188d26aed39e977b81c11650bfc99c314743b
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-05-07 21:53:29 +0900

    basic configurations

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   get 'home/index'
   post 'search' => 'search#search'
   root :to => 'home#index'
+
+  
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
