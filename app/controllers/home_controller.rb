7fc5835a41d5300bd62a0756bdf5d9ccf269890e
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-10-03 18:36:19 +0900

    new posts layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     @popular_books=Book.joins(:reviews).group(:id).order('count(user_id) desc').limit(4)
     @books= Book.order(created_at:"DESC").limit(4)
     @reviews= Review.order(created_at: "DESC").limit(4)
-
+    @related_posts = RelatedPost.order(created_at: "DESC").limit(4)
   end
 end
