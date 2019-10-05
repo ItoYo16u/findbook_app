e037d8daf104ff3d2f9d350a067771872231bf5e
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-10-05 22:18:01 +0900

    show self post on timeline

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,6 +2,7 @@ class TimelineController < ApplicationController
     before_action :authenticate_user!
     def index
         followings = UserUser.where(user_id:current_user.id).pluck(:follow_id)
+        followings << current_user.id
         @reviews = Review.order(created_at:"DESC").where(user_id:followings).page(params[:page]).per(15)
     end
 end
