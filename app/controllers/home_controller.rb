76f3ec1829a29b5716495b59dde3477ba8401a62
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-10-08 01:16:54 +0900

    change col num

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     @popular_books=Book.joins(:reviews).group(:id).order('count(user_id) desc').limit(5)
-    @popular_design_books = Book.where(genre:"デザイン").joins(:reviews).group(:id).order('count(user_id) desc').limit(4)
-    @popular_tech_books = Book.where(genre:"テクノロジー").joins(:reviews).group(:id).order('count(user_id) desc').limit(4)
-    @popular_biz_books = Book.where(genre:"ビジネス").joins(:reviews).group(:id).order('count(user_id) desc').limit(4)
-    @popular_humanity_books = Book.where(genre:"アート/カルチャー").joins(:reviews).group(:id).order('count(user_id) desc').limit(4)
-    @popular_academic_books = Book.where(genre:"アカデミック").joins(:reviews).group(:id).order('count(user_id) desc').limit(4)
+    @popular_design_books = Book.where(genre:"デザイン").joins(:reviews).group(:id).order('count(user_id) desc').limit(3)
+    @popular_tech_books = Book.where(genre:"テクノロジー").joins(:reviews).group(:id).order('count(user_id) desc').limit(3)
+    @popular_biz_books = Book.where(genre:"ビジネス").joins(:reviews).group(:id).order('count(user_id) desc').limit(3)
+    @popular_humanity_books = Book.where(genre:"アート/カルチャー").joins(:reviews).group(:id).order('count(user_id) desc').limit(3)
+    @popular_academic_books = Book.where(genre:"アカデミック").joins(:reviews).group(:id).order('count(user_id) desc').limit(3)
     @books= Book.order(created_at:"DESC").limit(6)
     @reviews= Review.order(created_at: "DESC").limit(4)
     @related_posts = RelatedPost.order(created_at: "DESC").limit(4)
