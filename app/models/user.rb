5774a6d1d0b546f62b9870d9840d58b52e339e44
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-10-08 21:25:38 +0900

    mailer setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:confirmable
   has_many :reviews, dependent: :destroy
   has_many :books, through: :reviews
   has_many :user_users,dependent: :destroy
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,17 +65,17 @@ Rails.application.configure do
   # SendGrid Configuration
   # heroku addons:create sendgrid:$plan
   # run "$ heroku config" to find sendgrid user_name and password
-  # config.action_mailer.default_url_options ={host:"yourApp.herokuapp.com"}
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings ={
-  # user_name:ENV["SENDGRID_USER_NAME"],
-  # password:ENV["SENDGRID_`PASSWORD"],
-  # domain:"heroku.com",# or your own domain
-  # adress:"smtp.sendgrid.net",
-  # port:587,#default
-  # authentication: :plain,
-  # enable_starttls_auto:true,
-  #}
+  config.action_mailer.default_url_options ={host:"findbook-app-test.herokuapp.com"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings ={
+  user_name:ENV["SENDGRID_USER_NAME"],
+  password:ENV["SENDGRID_`PASSWORD"],
+  domain:"heroku.com",# or your own domain
+  adress:"smtp.sendgrid.net",
+  port:587,#default
+  authentication: :plain,
+  enable_starttls_auto:true,
+  }
 
 
 
