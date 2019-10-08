aa96bf732f56edeb3b9a2db74e175b2809bef3ff
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-10-09 01:34:05 +0900

    bug fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,6 @@ class HomeController < ApplicationController
     @popular_academic_books = Book.where(genre:"アカデミック").joins(:reviews).group(:id).order('count(user_id) desc').limit(3)
     @books= Book.order(created_at:"DESC").limit(6)
     @reviews= Review.order(created_at: "DESC").limit(4)
-    @related_posts = RelatedPost.order(created_at: "DESC").limit(8))
+    @related_posts = RelatedPost.order(created_at: "DESC").limit(8)
   end
 end
