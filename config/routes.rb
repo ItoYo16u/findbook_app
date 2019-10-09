a6435ff198197cc95f91821ba1475664290f542f
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-10-09 12:01:32 +0900

    devise veiws update

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
     devise_for :users, controllers: {
       sessions: 'users/sessions',
       registrations: 'users/registrations',
+      confirmations:'users/confirmations',
       mailer: 'users/mailer',
       "users/show" => "users#show",
     }
