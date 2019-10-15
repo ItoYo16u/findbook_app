2b57fbad83c9107e3d1d0ab1780852d93b449b82
ItoYo16u <ito.yo16uh90616@gmail.com>
2019-10-15 15:31:19 +0900

    domainsetting

--- config/environment.rb
+++ config/environment.rb
@@ -15,6 +15,6 @@ ActionMailer::Base.smtp_settings = {
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'bookio.info',
     :enable_starttls_auto => true
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,13 +65,13 @@ Rails.application.configure do
   # SendGrid Configuration
   # heroku addons:create sendgrid:$plan
   # run "$ heroku config" to find sendgrid user_name and password
-  config.action_mailer.default_url_options ={ host:"findbook-app-test.herokuapp.com"}
+  config.action_mailer.default_url_options ={ host:"bookio.info"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings ={
   user_name:ENV["SENDGRID_USERNAME"],
   password:ENV["SENDGRID_PASSWORD"],
-  domain:"heroku.com",
+  domain:"bookio.info",
   adress:"smtp.sendgrid.net",
   port:587,
   authentication: :plain,
