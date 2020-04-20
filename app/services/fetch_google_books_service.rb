b5ccdccd4dabd668640beaa5bf0f93891cd74fe3
ItoYo16u <ito.yo16uh90616@gmail.com>
2020-04-21 00:35:36 +0900

    add comment

--- app/services/fetch_google_books_service.rb
+++ app/services/fetch_google_books_service.rb
@@ -6,20 +6,35 @@ class GoogleBooksService
 
   class << self
     def fetch_books_by(keyword)
+
+      # TODO implement keyword validator
+      if keyword.empty?
+        return {
+          status: 'Failure',
+          message: "at least one keyword required",
+          data: {}
+        }.to_json
+      end
+
       # TODO: use array args and parse it
       #       or option parameters
       uri = google_api_url + "#{keyword}"
-      encoded_uri = URI.encode_www_form(uri)
+      encoded_uri = URI.encode(uri)
       parsed_uri = URI.parse(encoded_uri)
       begin
         res = Net::HTTP.get(parsed_uri)
-        JSON.parse(res)
       rescue => exception
         #TODO implement this
       end
+      JSON.parse(res)
     end
 
     def fetch_book_by(isbn_code)
+      # TODO: implement isbn validator
+      #       requirement
+      #       raise error when isbn_code is empty
+      #       raise error when isbn.length != 11 or 13
+      
       uri = google_api_url + "isbn#{isbn_code}"
       encoded_uri = URI.encode_www_form(uri)
       parsed_uri = URI.parse(encoded_uri)
